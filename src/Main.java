bfd2930d61632ae0658abe28b34abe6f010aaaba
Jim jtulip-imac <jtulip@csu.edu.au>
2015-07-23 00:23:44 +1000

    got a basic demo working

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.DriverManager;
+import java.sql.Timestamp;
 
 public class Main {
 
+  
+  
 	public static void main(String[] args) {
 		String driver = "org.apache.derby.jdbc.EmbeddedDriver";
 		String dbName="derbyDemo";
@@ -24,33 +30,83 @@ public class Main {
 		                    + " CONSTRAINT WISH_PK PRIMARY KEY, "
 		                    + " ENTRY_DATE TIMESTAMP DEFAULT CURRENT_TIMESTAMP, "
 		                    + " WISH_ITEM VARCHAR(32) NOT NULL)";
-		String answer;
 
 		try {
-		  //try connecting to existing DB 
+		  // try connecting to existing DB 
 			conn = connectToExistingDB(connectionURL);
 			if (conn == null) {
-			  //if that doesn't work, creat the database
+			  // if that doesn't work, create the database
 				System.out.println(dbName + " does not exist, creating ...");
-				conn = DriverManager.getConnection(creationURL);
-				
-				//and set up the table
-				s = conn.createStatement();
-				s.execute(createTableString);				
-			}
+				conn = DriverManager.getConnection(creationURL);				
+			}			
+      System.out.println("Connected to database: " + dbName);
+      s = conn.createStatement();
+      
+      // check for valid table
+      if (!chk4Table(conn)) {
+      // and set it up if it doesn't exit
+        s.execute(createTableString);               
+      }
+      System.out.println("WISH_LIST table OK");
+      
+			// now we should have a valid database and table structure
 			
-			//now we should have a database and a table structure
-			System.out.println("Connected to database: " + dbName);
-			chk4Table(conn);
-			System.out.println("WISH_LIST table OK");
+			// setup the prepared statement
+			psInsert = conn.prepareStatement("INSERT INTO WISH_LIST(WISH_ITEM) VALUES (?)");
+			
+			// now run the add/list record loop
+			String answer ="";
+			while (!answer.equals("q")) {
+			  answer = getWishItem();
+			  if (answer.equals("q")) break;
+			  
+			  // update the prepared statement with the wish item
+			  psInsert.setString(1, answer);
+			  // insert wish item into table
+			  psInsert.executeUpdate();
+			  
+			  // Now list all the records
+			  myWishes = s.executeQuery("SELECT ENTRY_DATE, WISH_ITEM " +
+			                            "FROM WISH_LIST " +
+			                            "ORDER BY ENTRY_DATE");
+			  System.out.println("--------------------------------------");
+			  while (myWishes.next()) {
+			    Timestamp dateTime = myWishes.getTimestamp(1);
+			    String wish = myWishes.getString(2);
+			    
+			    System.out.println("On " + dateTime + " I wished for a " + wish);
+			  }
+        System.out.println("--------------------------------------");
+			  myWishes.close();
+			}
+			// clean up
+      System.out.println("Exiting.");
+			psInsert.close();
+			s.close();
+			conn.close();
+			
+			//shut down derby (only for embedded mode)
+			try {
+			  DriverManager.getConnection("jdbc:derby:;shutdown=true");
+			}
+			catch (SQLException sqle) {
+			  if (!sqle.getSQLState().equals("XJ015")) {
+			    System.err.println("DB did not shutdown normally");
+			  }
+			  else {
+          System.out.println("DB shutdown normally");
+			  }
+			}
 		}
 		catch (Throwable e) {
-			System.err.println(". . . Exception thrown:");
+			System.err.println(". . . Exception thrown:" + e);
 			e.printStackTrace(System.err);
 		}
 	}
 	
-	public static Connection connectToExistingDB(String connectionURL) throws SQLException {
+	
+	
+	private static Connection connectToExistingDB(String connectionURL) throws SQLException {
 		Connection conn = null;
 		try {
 			conn = DriverManager.getConnection(connectionURL);
@@ -62,7 +118,10 @@ public class Main {
 		}
 		return conn;
 	}
-	public static boolean chk4Table(Connection conn) throws SQLException {
+	
+	
+	
+	private static boolean chk4Table(Connection conn) throws SQLException {
 		try {
 			Statement s = conn.createStatement();
 			s.execute("UPDATE WISH_LIST " +
@@ -85,6 +144,24 @@ public class Main {
 			}
 		}
 		return true;
+	}
+	
+	
+	
+	private static String getWishItem() throws IOException {
+	  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	  String ans = "";
+	  try {
+	    while (ans.length() == 0) {
+	      System.out.println("Enter wish (q to exit): ");
+	      ans = br.readLine();
+	    }
+	  }
+	  catch (IOException ioe) {
+	    System.err.println("Could not read from System.in");
+	    throw ioe;
+	  }
+	  return ans;
 	}
 
 }
