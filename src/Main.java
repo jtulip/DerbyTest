90ed623f4e06ff70870fdc3dd598402959e8bf68
Jim jtulip-imac <jtulip@csu.edu.au>
2015-07-23 16:38:12 +1000

    got menu working

--- src/Main.java
+++ src/Main.java
@@ -22,8 +22,12 @@ public class Main {
 		
 		Connection conn = null;
 		Statement s = null;
-		PreparedStatement psInsert = null;
-		ResultSet myWishes;
+    PreparedStatement psCreate = null;
+    PreparedStatement psRead = null;
+    PreparedStatement psUpdate = null;
+    PreparedStatement psDelete = null;
+    PreparedStatement psList = null;
+		ResultSet rs;
 		
 		String createTableString = "CREATE TABLE WISH_LIST" 
 		                    + " (WISH_ID INT NOT NULL GENERATED ALWAYS AS IDENTITY"
@@ -49,38 +53,51 @@ public class Main {
         s.execute(createTableString);               
       }
       System.out.println("WISH_LIST table OK");
-/**
+
 			// now we should have a valid database and table structure
 			
-			// setup the prepared statement
-			psInsert = conn.prepareStatement("INSERT INTO WISH_LIST(WISH_ITEM, WISH_DESC) VALUES (?, ?)");
+			// setup the prepared statements
+			psCreate = conn.prepareStatement("INSERT INTO WISH_LIST(WISH_ITEM, WISH_DESC) VALUES (?, ?)");
+			psRead   = conn.prepareStatement("SELECT * FROM WISH_LIST WHERE WISH_ITEM='?'");
+			psList   = conn.prepareStatement("SELECT * FROM WISH_LIST");
+			String menu = "\n    Enter selection: \n" +
+          "         C - create a new record\n" +
+          "         R - read a record\n" +
+          "         U - update a record\n" +
+          "         D - delete a  record\n" +
+          "         L - list all records\n" +
+          "     Selection: ";
 			
 			// now run the add/list record loop
 			String answer ="";
 			while (!answer.equals("q")) {
-			  answer = getWishItem();
+			  answer = getInput(menu);
 			  if (answer.equals("q")) break;
 			  
-			  // update the prepared statement with the wish item
-			  psInsert.setString(1, answer);
-			  // insert wish item into table
-			  psInsert.executeUpdate();
+			  switch (answer) {
 			  
-			  // Now list all the records
-			  myWishes = s.executeQuery("SELECT ENTRY_DATE, WISH_ITEM " +
-			                            "FROM WISH_LIST " +
-			                            "ORDER BY ENTRY_DATE");
-			  System.out.println("--------------------------------------");
-			  while (myWishes.next()) {
-			    Timestamp dateTime = myWishes.getTimestamp(1);
-			    String wish = myWishes.getString(2);
-			    
-			    System.out.println("On " + dateTime + " I wished for a " + wish);
+        case "c":
+        case "C":  createRecord(s);
+                   break;
+        case "r":
+        case "R":  readRecord(s);
+                   break;
+        case "u":
+        case "U":  updateRecord(s);
+                   break;
+        case "d":
+        case "D":  deleteRecord(s);
+                   break;
+        case "l":
+        case "L":  listRecords(s);
+                   break;
+			    case "q":
+			    case "Q":  break;
+			    default:   System.out.println("Unknown selection");
+			  
 			  }
-        System.out.println("--------------------------------------");
-			  myWishes.close();
 			}
-**/
+
 			// clean up
       System.out.println("Exiting.");
 			//psInsert.close();
@@ -105,9 +122,48 @@ public class Main {
 			e.printStackTrace(System.err);
 		}
 	}
+
 	
 	
-	
+  private static void createRecord(Statement s) {
+    System.out.println("Creating Record");
+  }
+
+  
+  
+  private static void readRecord(Statement s) {
+    System.out.println("Reading Record");
+  }
+
+  
+  
+  private static void updateRecord(Statement s) {
+    System.out.println("Updating Record");
+  }
+
+  
+  
+  private static void deleteRecord(Statement s) {
+    System.out.println("Deleting Record");
+  }
+
+  
+  
+  private static void listRecords(Statement s) throws SQLException {
+    System.out.println("Listing Records");
+    ResultSet rs = s.executeQuery("SELECT * FROM WISH_LIST");
+    while (rs.next()) {
+      Timestamp dateTime = rs.getTimestamp(2);
+      String item = rs.getString(3);
+      String desc = rs.getString(4);
+
+      System.out.println("On " + dateTime + " I wished for a " + desc + " " + item);      
+    }
+    rs.close();
+  }
+
+  
+  
 	private static Connection connectToExistingDB(String connectionURL) throws SQLException {
 		Connection conn = null;
 		try {
@@ -150,13 +206,13 @@ public class Main {
 	
 	
 	
-	private static String getWishItem() throws IOException {
+	private static String getInput(String prompt) throws IOException {
 	  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	  String ans = "";
 	  try {
 	    while (ans.length() == 0) {
-	      System.out.println("Enter wish (q to exit): ");
-	      ans = br.readLine();
+	      System.out.print(prompt);
+	      ans = br.readLine().trim();
 	    }
 	  }
 	  catch (IOException ioe) {
