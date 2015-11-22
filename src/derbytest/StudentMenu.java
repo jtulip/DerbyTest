599c7aec6d60e6f9b57e9e45f9f09216a89390e2
Jim <jtulip@csu.edu.au>
2015-11-23 10:01:36 +1100

    all CRUD operations for Students, plus reformatted output

--- src/derbytest/StudentMenu.java
+++ src/derbytest/StudentMenu.java
@@ -11,24 +11,49 @@ public class StudentMenu {
     private static BufferedReader in_;
     
     
+
+    /**
+     * Display and process the available menu options for the Student table
+     * 
+     */
     public static void execute(ClientConnectionPoolDataSource ds, BufferedReader in) {
-        // menu display loop
-    
         ds_ = ds;
         in_ = in;
         
+        String menu = String.format(                
+                "\n\nSelect :\n" +
+                        "\tC: create Student\n" +
+                        "\tR: read Student\n" +
+                        "\tU: update Student\n" +
+                        "\tD: delete Student\n" +
+                        "\tL: list all Students\n" +
+                        "\n\tX: return to main\n" +
+                "\n" +
+                "Selection : "
+                );
+        
+        // menu display loop
         boolean stop = false;
         
         while (!stop) {
             try {
-                displayMenu();
-                String ans = in.readLine();
+                System.out.print(menu);
+                String ans = in_.readLine();
                 
                 switch (ans.toUpperCase()) {
                 
-                    case "C": create();
-                    break;  
-          
+                	case "C": create();
+                	          break;  
+      
+	                case "R": read();
+                              break;
+                    
+	                case "U": update();
+                              break;  
+                              
+	                case "D": delete();
+	                		  break;
+	      
                     case "L": list();
                               break;  
                     
@@ -45,97 +70,227 @@ public class StudentMenu {
         System.out.println("\nReturning to Main.");
     }
 
+ 
     
-    
-    private static void displayMenu() {
-        System.out.print(                
-                "\n\nSelect :\n" +
-                        "\tC: create Student\n" +
-                        "\tR: read Student\n" +
-                        "\tU: update Student\n" +
-                        "\tD: delete Student\n" +
-                        "\tL: list all Student\n" +
-                        "\n\tX: return to main\n" +
-                "\n" +
-                "Selection : "
-                );
+    /**
+     * Delete a student. 
+     * 
+     * @throws SQLException
+     */
+    private static void delete() throws IOException, SQLException {
+        //get student id
+        int sid = getStudentId();
+                
+        String raw = String.format("DELETE FROM Students WHERE StudentId = %d", sid);
+        try (Connection con = ds_.getConnection();
+             Statement sta = con.createStatement(); ) {
+        	
+        	sta.execute(raw);
+        }        
     }
 
     
+    
     /**
-     * Lists all rows in the table
+     * Update a student. Only first name and last name may be changed.
      * 
      * @throws SQLException
      */
-    private static void list() throws SQLException {
+    private static void update() throws IOException, SQLException {
+        //get student id
+        int sid = getStudentId();
+                
+        String raw = String.format("SELECT * FROM Students WHERE StudentId = %d", sid);
         try (Connection con = ds_.getConnection();
              Statement sta = con.createStatement();
-             ResultSet res = sta.executeQuery( "SELECT * FROM Students"); ) {
+             ResultSet res = sta.executeQuery(raw); ) {
         
-            while (res.next()) {
-                System.out.println(res.getInt("StudentId") + "    " + res.getString("FirstName") + "    " + res.getString("LastName"));
-            }
+			if (res.next()) {
+				System.out.println("\nOriginal record: ");
+				System.out.println(String.format("\n\t%s\t%s\t%s", "StudentId", "FirstName","LastName"));
+				System.out.println(String.format("\t%d\t\t%s\t\t%s", 
+						res.getInt("StudentId"), res.getString("FirstName"), res.getString("LastName")));
+			}
+        	else {
+        		System.out.println(String.format("No such student: %d", sid));
+        		return;
+        	}
+        }
+        
+        //get new first name
+        String fname = getFirstName();
+        //get new last name
+        String lname = getLastName();
+        
+        //update record
+        raw = String.format("UPDATE Students SET FirstName='%s', LastName='%s' WHERE StudentId=%d", fname, lname, sid);
+        try (Connection con = ds_.getConnection();
+             Statement sta = con.createStatement(); ) {
+        
+            sta.execute(raw);
+        }
+
+        raw = String.format("SELECT * FROM Students WHERE StudentId = %d", sid);
+        try (Connection con = ds_.getConnection();
+             Statement sta = con.createStatement();
+             ResultSet res = sta.executeQuery(raw); ) {
+        
+			if (res.next()) {
+				System.out.println("\nUpdated record: ");
+				System.out.println(String.format("\n\t%s\t%s\t%s", "StudentId", "FirstName","LastName"));
+				System.out.println(String.format("\t%d\t\t%s\t\t%s", 
+						res.getInt("StudentId"), res.getString("FirstName"), res.getString("LastName")));
+			}
+        }
+    }
+    
+
+    
+    /**
+     * Read a row in the table
+     * 
+     * @throws SQLException
+     */
+    private static void read() throws IOException, SQLException {
+        //get student id
+        int sid = getStudentId();
+        
+        String raw = String.format("SELECT * FROM Students WHERE StudentId = %d", sid);
+        try (Connection con = ds_.getConnection();
+             Statement sta = con.createStatement();
+             ResultSet res = sta.executeQuery(raw); ) {
+        
+			System.out.println(String.format("\n\t%s\t%s\t%s", "StudentId", "FirstName","LastName"));
+			while (res.next()) {
+				System.out.println(String.format("\t%d\t\t%s\t\t%s", 
+						res.getInt("StudentId"), res.getString("FirstName"), res.getString("LastName")));
+			}
         }
     }
     
     
     
-    /**
-     * Creates a new record in the Students table
-     * 
-     * @throws SQLException
-     */
+   /**
+    * Lists all rows in the table
+    * 
+    * @throws SQLException
+    */
+    private static void list() throws SQLException {
+    	String raw = String.format("SELECT * FROM Students");
+    	try (Connection con = ds_.getConnection();
+    			Statement sta = con.createStatement();
+    			ResultSet res = sta.executeQuery(raw); ) {
+   
+			System.out.println(String.format("\n\t%s\t%s\t%s", "StudentId", "FirstName","LastName"));
+			while (res.next()) {
+				System.out.println(String.format("\t%d\t\t%s\t\t%s", 
+						res.getInt("StudentId"), res.getString("FirstName"), res.getString("LastName")));
+			}
+    	}
+    }
+   
+   
+   
+	/**
+	 * Creates a new record in the Students table
+	 * 
+	 * @throws SQLException
+	 */
     private static void create() throws IOException, SQLException {        
         //get student id
-        System.out.print("Enter new StudentID: ");
-        int sid = 0;
-        while ( sid <= 0 ) {
-            try {
-                sid = Integer.parseInt(in_.readLine());
-                if (sid <= 0 ) {
-                    System.out.println("Invalid input - id must be integer > 0");
-                }
-            }
-            catch (NumberFormatException nfe) {
-                System.out.println("Invalid input - id must be integer > 0");
-            }
-        }
+    	int sid = getStudentId();
         
         //get firstname
-        System.out.print("Enter first name: ");
-        String fname = "";
-        while (true) {
-            fname = in_.readLine().trim();
-            if ( 0 == fname.length() || fname.length() > 32 || !fname.matches("[a-zA-Z]+")) {
-                System.out.println("Invalid input - fname must be alpha only, 0 < length < 32");
-            }
-            else {
-                break;
-            }
-        }
+    	String fname = getFirstName();
         
         //get lastname
-        System.out.print("Enter last name: ");
-        String lname = "";
-        while (true) {
-            lname = in_.readLine().trim();
-            if (lname.length() == 0 || lname.length() > 32 || !fname.matches("[a-zA-Z]+")) {
-                System.out.println("Invalid input - lname must be alpha only, 0 < length < 32");
-            }
-            else {
-                break;
-            }
-        }
+        String lname = getLastName();
         
+        String raw = String.format("INSERT INTO Students VALUES (%d, '%s', '%s')", sid, fname, lname);
         try (Connection con = ds_.getConnection();
              Statement sta = con.createStatement(); ) {
         
-            sta.execute(String.format("INSERT INTO Students VALUES (%d, '%s', '%s')", sid, fname, lname));
+            sta.execute(raw);
         }
         catch (SQLIntegrityConstraintViolationException e) {
             System.out.print(String.format("Invalid StudentId - %d already exists", sid));            
         }
     }
+ 
     
+    
+    /**
+     * Internal utility method to return a student ID
+     * 
+     * @throws IOException
+     * @return the student id
+     */   
+    private static int getStudentId() throws IOException {
+	    //get student id
+	    System.out.print("Enter StudentID: ");
+	    int sid = 0;
+	    while ( sid <= 0 ) {
+	        try {
+	            sid = Integer.parseInt(in_.readLine());
+	            if (sid <= 0 ) {
+	                System.out.println("Invalid input - id must be integer > 0");
+	            }
+	        }
+	        catch (NumberFormatException nfe) {
+	            System.out.println("Invalid input - id must be integer > 0");
+	        }
+	    }
+	    return sid;
+    }
+    
+    
+    
+    /**
+     * Internal utility method to return a first name
+     * 
+     * @throws IOException
+     * @return the student's first name
+     */   
+    private static String getFirstName() throws IOException {	    
+	    String fname = getName("Enter first name: ");
+	    return fname;   
+    }
+    
+    
+    
+    /**
+     * Internal utility method to return a last name
+     * 
+     * @throws IOException
+     * @return the student's last name
+     */   
+    private static String getLastName() throws IOException {	    
+	    String lname = getName("Enter last name: ");
+	    return lname;   
+    }
+    
+    
+    
+    /**
+     * Internal utility method to return a any name
+     * 
+     * @throws IOException
+     * @return the name
+     */   
+    private static String getName(String prompt)  throws IOException {	    
+	    //get name
+	    System.out.print(prompt);
+	    String name = "";
+	    while (true) {
+	        name = in_.readLine().trim();
+	        if ( 0 == name.length() || name.length() > 32 || !name.matches("[a-zA-Z]+")) {
+	            System.out.println("Invalid input - name must be alpha only, 0 < length < 32");
+	        }
+	        else {
+	            break;
+	        }
+	    }
+	    return name;   
+    }
     
 }
