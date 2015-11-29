68c6669ccb85ed597d7a262077ff804db5386e0a
Jim <jtulip@csu.edu.au>
2015-11-29 18:09:22 +1100

    modified schema file to include on delete cascades for foreign keys and utility triggers for marks

--- CourseManager_Schema.sql
+++ CourseManager_Schema.sql
@@ -1,5 +1,4 @@
 -- /* Create Students table */
-
 CREATE TABLE Students
 	( 
 		StudentId integer NOT NULL,
@@ -13,50 +12,57 @@ ALTER TABLE Students
 
 
 -- /* Create Subjects table */
-
 CREATE TABLE Subjects
 	(
 		SubjectCode character(6) NOT NULL,
 		SubjectName varchar(64) NOT NULL
 	);
+
 ALTER TABLE Subjects
 	ADD CONSTRAINT pk_Subjects PRIMARY KEY (SubjectCode);
 
 	
 
 -- /* Create Enrolments table */
-
 CREATE TABLE Enrolments
 	(
 		SubjectCode character(6) NOT NULL,
 		StudentId integer NOT NULL
 	);
+
 ALTER TABLE Enrolments
 	ADD CONSTRAINT pk_Enrolments PRIMARY KEY (SubjectCode, StudentId);
+
 ALTER TABLE Enrolments
-	ADD CONSTRAINT Enrol_Subjects_fk FOREIGN KEY (SubjectCode) REFERENCES Subjects (SubjectCode);
+	ADD CONSTRAINT Enrol_Subjects_fk FOREIGN KEY (SubjectCode) 
+		REFERENCES Subjects (SubjectCode);
+
 ALTER TABLE Enrolments
-	ADD CONSTRAINT Enrol_Students_fk FOREIGN KEY (StudentId) REFERENCES Students (StudentId);
+	ADD CONSTRAINT Enrol_Students_fk FOREIGN KEY (StudentId) 
+		REFERENCES Students (StudentId)
+		ON DELETE CASCADE;;
 
 	
 
 -- /* Create Assessments table */
-
 CREATE TABLE Assessments
 	(
 		SubjectCode character(6) NOT NULL,
 		AssessmentCode character(4) NOT NULL,
 		AssessmentWeight integer NOT NULL
 	);
+
 ALTER TABLE Assessments
 	ADD CONSTRAINT pk_Assessments PRIMARY KEY (SubjectCode, AssessmentCode);
+
 ALTER TABLE Assessments
-	ADD CONSTRAINT Assess_Subjects_fk FOREIGN KEY (SubjectCode) REFERENCES Subjects (SubjectCode);
+	ADD CONSTRAINT Assess_Subjects_fk FOREIGN KEY (SubjectCode) 
+		REFERENCES Subjects (SubjectCode)
+		ON DELETE CASCADE;
 
 
 
 -- /* Create Marks table */
-
 CREATE TABLE Marks
 	(
 		StudentId integer NOT NULL,
@@ -66,14 +72,49 @@ CREATE TABLE Marks
 	);
 ALTER TABLE Marks
 	ADD CONSTRAINT pk_Marks PRIMARY KEY (StudentId, SubjectCode, AssessmentCode);
+
 ALTER TABLE Marks
-	ADD CONSTRAINT Students_fk FOREIGN KEY (StudentId) REFERENCES Students (StudentId);
+	ADD CONSTRAINT Mark_Students_fk FOREIGN KEY (StudentId) 
+		REFERENCES Students (StudentId)
+		ON DELETE CASCADE;
+
 ALTER TABLE Marks
-	ADD CONSTRAINT Assessments_fk FOREIGN KEY (SubjectCode, AssessmentCode) 
-		REFERENCES Assessments (SubjectCode, AssessmentCode);
+	ADD CONSTRAINT Mark_Subjects_fk FOREIGN KEY (SubjectCode) 
+		REFERENCES Subjects (SubjectCode)
+		ON DELETE CASCADE;
+
+ALTER TABLE Marks
+	ADD CONSTRAINT Mark_Enrolments_fk FOREIGN KEY (SubjectCode, StudentId) 
+		REFERENCES Enrolments (SubjectCode, StudentId)
+		ON DELETE CASCADE;
+
+ALTER TABLE Marks
+	ADD CONSTRAINT Mark_Assessments_fk FOREIGN KEY (SubjectCode, AssessmentCode) 
+		REFERENCES Assessments (SubjectCode, AssessmentCode)
+		ON DELETE CASCADE;
+
 ALTER TABLE Marks
 	ADD CONSTRAINT Mark_In_Range CHECK (Mark >= 0.0 AND Mark <= 100.0);
 
+	
+CREATE TRIGGER On_Add_Assessment AFTER INSERT ON Assessments 
+	REFERENCING New AS New_Assessment FOR EACH ROW MODE DB2SQL
+		INSERT INTO Marks
+			SELECT Enrolments.StudentId, Enrolments.SubjectCode, New_Assessment.AssessmentCode, 0.0 
+			FROM Enrolments INNER JOIN Assessments ON Enrolments.SubjectCode = Assessments.SubjectCode
+			WHERE Enrolments.SubjectCode = New_Assessment.SubjectCode AND Assessments.AssessmentCode = New_Assessment.AssessmentCode;
+	
+	
+CREATE TRIGGER On_Insert_Enrolment AFTER INSERT ON Enrolments 
+	REFERENCING New AS New_Enrolment FOR EACH ROW MODE DB2SQL
+		INSERT INTO Marks
+			SELECT Enrolments.StudentId, Enrolments.SubjectCode, Assessments.AssessmentCode, 0.0 
+			FROM Enrolments INNER JOIN Assessments ON Enrolments.SubjectCode = Assessments.SubjectCode
+			WHERE Enrolments.SubjectCode = New_Enrolment.SubjectCode AND Enrolments.StudentId = New_Enrolment.StudentId;
+	
+-- CREATE TRIGGER On_Delete_Enrolment AFTER DELETE ON Enrolments 
+--	REFERENCING Old AS Old_Enrolment FOR EACH ROW MODE DB2SQL
+--		DELETE FROM Marks WHERE Marks.SubjectCode = Old_Enrolment.SubjectCode AND Marks.StudentId = Old_Enrolment.StudentId;
 
 -- /* Insert test data */
 
@@ -89,19 +130,6 @@ INSERT INTO Students VALUES
 	(4, 'Fred', 'Nurke'),
 	(5, 'Edward', 'Seagoon');
 	
-INSERT INTO Enrolments VALUES 
-	('ITC203',1), 
-	('ITC203',3), 
-	('ITC203',4),
-	('ITC203',5),
-	('ITC205',1),
-	('ITC205',2),
-	('ITC205',4),
-	('ITC205',5),
-	('ITC303',3),
-	('ITC303',5),
-	('ITC303',2);
-	
 INSERT INTO Assessments VALUES 
 	('ITC203','Asg1',25), 
 	('ITC203','Asg2',25), 
@@ -114,21 +142,17 @@ INSERT INTO Assessments VALUES
 	('ITC303','PREM',25),
 	('ITC303','PMAS',25);
 
-INSERT INTO Marks VALUES 
-	(1,'ITC203','Asg1',0), 
-	(1,'ITC203','Asg2',0), 
-	(1,'ITC203','Asg3',0), 
-	(1,'ITC203','Prac',0),
-	(4,'ITC203','Asg1',0), 
-	(4,'ITC203','Asg2',0), 
-	(4,'ITC203','Asg3',0), 
-	(4,'ITC203','Prac',0),
-	(5,'ITC203','Asg1',0), 
-	(5,'ITC203','Asg2',0), 
-	(5,'ITC203','Asg3',0), 
-	(5,'ITC203','Prac',0),
-	(3,'ITC203','Asg1',0), 
-	(3,'ITC203','Asg2',0), 
-	(3,'ITC203','Asg3',0), 
-	(3,'ITC203','Prac',0);
-
+	
+	INSERT INTO Enrolments VALUES 
+	('ITC203',1), 
+	('ITC203',3), 
+	('ITC203',4),
+	('ITC203',5),
+	('ITC205',1),
+	('ITC205',2),
+	('ITC205',4),
+	('ITC205',5),
+	('ITC303',3),
+	('ITC303',5),
+	('ITC303',2);
+	
